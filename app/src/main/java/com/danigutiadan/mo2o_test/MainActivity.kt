20a7f4a6490c88d40b00c750adc25bbc8bafe1f4
Daniel Gutiérrez Adán <dani@MBP-de-Daniel.lan>
2022-11-07 14:50:31 +0100

    started implenting navigation fragment

--- app/src/main/java/com/danigutiadan/mo2o_test/MainActivity.kt
+++ app/src/main/java/com/danigutiadan/mo2o_test/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.danigutiadan.mo2o_test
-
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import com.danigutiadan.mo2o_test.databinding.ActivityMainBinding
 import com.danigutiadan.mo2o_test.ui.BaseActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : BaseActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        navController = findNavController(R.id.nav_host_fragment)
+        //setSupportActionBar(binding.toolbar)
+        //NavigationUI.setupActionBarWithNavController(this, navController)
     }
 }
---
+++ app/src/main/java/com/danigutiadan/mo2o_test/features/beer/BeerActivity.kt
@@ -0,0 +1,30 @@
+package com.danigutiadan.mo2o_test.features.beer
+
+
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.danigutiadan.mo2o_test.R
+import com.danigutiadan.mo2o_test.databinding.ActivityBeerBinding
+import com.danigutiadan.mo2o_test.ui.BaseActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class BeerActivity : BaseActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var binding: ActivityBeerBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityBeerBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        navController = findNavController(R.id.nav_host_fragment)
+        setSupportActionBar(binding.toolbar)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+}
